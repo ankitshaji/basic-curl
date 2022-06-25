0f767dac834ddca9d32301048d86ed9bdde0b45f
Ankit Shaji <ankitshaji@gmail.com>
2022-06-25 18:43:47 +1200

    if header required

--- curlput.php
+++ curlput.php
@@ -7,30 +7,37 @@ $ch = curl_init();
 $url = "https://reqres.in/api/users/2";
 
 //data to send - array format
-$data_array= array(
-    'name'=>'Updated Test Name',
-    'job'=> 'Updated Test Job'
+$data_array = array(
+    'name' => 'Updated Test Name',
+    'job' => 'Updated Test Job'
 );
 //array to url format
 $data = http_build_query($data_array);
 
+//if header required - header to send - array format
+$header = array(
+    "Authorization: infohere"
+);
+
 //curl options
-curl_setopt($ch,CURLOPT_URL,$url);
-curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"PUT"); //Set PUT request  
-curl_setopt($ch,CURLOPT_POSTFIELDS,$data);//Set data to send
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);//return respose instead of outputting
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT"); //Set PUT request  
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data); //Set data to send
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //return respose instead of outputting
+//if header is required
+curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
 //stores json response to variable
 $resp = curl_exec($ch);
 
 //error check
-if($e = curl_error($ch)){
+if ($e = curl_error($ch)) {
     echo $e;
-}else{
+} else {
     //json to php object ,not array false
     $decode = json_decode($resp);
     //iterate object with foreach
-    foreach($decode as $key => $val){
-        echo $key.":".$val.'<br>'; //. is +
+    foreach ($decode as $key => $val) {
+        echo $key . ":" . $val . '<br>'; //. is +
     }
 }
 //close connection
